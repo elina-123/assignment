dfe9f08e789533397280e7be16eaaf3e24788f8a
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-09-26 21:30:21 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,13 @@
+//Java Program to Add two Numbers
+
+public class Add {
+	public static void main(String[] args)
+	{
+	int a,b,c;
+	a= 5;
+	b=7;
+	c=a+b;
+	System.out.println("Addition of two number is : " + c);
+	}
+	
+}
