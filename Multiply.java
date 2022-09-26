e62e43b91447f69d8ae7f3be7a81a880883d06ab
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-09-26 22:25:08 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,13 @@
+//Java Program to Multiply Two Floating-Point Numbers
+
+class Multiply
+{
+public static void main(String[] args)
+{
+float a,b,c;
+a= 10.5f;
+b= 5.5f;
+c=a*b;
+System.out.println("Multiplication of 2 floating point numbers is : "+c);
+}
+}
