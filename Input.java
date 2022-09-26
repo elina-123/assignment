8fae0eb9de6c82eba8179aa2cb0803473078bb67
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-09-26 13:02:46 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,12 @@
+package Input.java;
+
+public class Input {
+	public static void main(String[] args)
+	{
+	Scanner s= new Scanner(System.in);
+	System.out.println("Put a number");
+	int a= s.nextInt();
+	System.out.println("The Input number is  : "+a);
+	}
+	
+}
