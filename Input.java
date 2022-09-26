ed2eaa88fa920af785048162374894571df720dd
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-09-26 22:16:09 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,15 @@
+
+// Get input from user
+
+import java.util.Scanner;
+
+class Input{
+
+public static void main(String[] args)
+{
+Scanner s= new Scanner(System.in);
+System.out.println("Put a number");
+int a= s.nextInt();
+System.out.println("The Input number is  : "+a);
+}
+}
