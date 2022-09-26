7a4343feab89d40bd5b2fd9edf66d8646d63c077
elina-123 <112066749+elina-123@users.noreply.github.com>
2022-09-26 22:10:14 +0530

    Add files via upload

---
+++ Multi.java
@@ -0,0 +1,13 @@
+//Java Program to Multiply two Numbers
+
+class Multi
+{
+public static void main(String[] args)
+{
+int a,b,c;
+a= 11;
+b=22;
+c=a*b;
+System.out.println("Addition of two number is : " + c);
+}
+}
